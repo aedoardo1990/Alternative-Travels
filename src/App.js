5a2b7a1937f2c8753e63fba3666b0149249a8a94
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-03-09 15:03:43 +0000

    create navbar, logo and favicon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Button from 'react-bootstrap/Button';
 import './App.css';
+import './components/NavBar.js';
+import NavBar from './components/NavBar.js';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello abandoned network!</h1>
-      <Button variant="primary">Primary</Button>
+      <NavBar/>
     </div>
   );
 }
