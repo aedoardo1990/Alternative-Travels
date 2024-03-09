b79d5d3e198d39a43eeebbc52f9f07ab7e14a4da
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-03-09 12:09:40 +0000

    add Bootstrap link in index.html and add title & button to test if it works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Button from 'react-bootstrap/Button';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hello abandoned network!</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
