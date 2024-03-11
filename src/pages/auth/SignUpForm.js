232063b10205dfa4ce14a65398a0383c39ffda4a
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-03-11 17:54:04 +0000

    create function handleChange and submitForm

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
@@ -6,8 +6,32 @@ import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
 import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import axios from "axios";
 
 const SignUpForm = () => {
+    const[signUpData, setSignUpData] = useState ({
+        username: '',
+        password1: '',
+        password2: ''
+    });
+const {username, password1, password2} = signUpData;
+
+const handleChange = (event) => {
+    setSignUpData({
+        ...signUpData,
+        [event.target.name]: event.target.value,
+    });
+}
+
+const submitForm = async (event) => {
+    event.preventDefault();
+    try {
+        await axios.post('dj-rest-auth/registration', signUpData)
+    } catch (err) {
+        
+    }
+}
+
     return (
         <Row className={styles.Row}>
             <Col className="my-auto py-2 p-md-2" md={6}>
@@ -17,17 +41,17 @@ const SignUpForm = () => {
                     <Form>
                         <Form.Group controlId="username">
                             <Form.Label className="d-none">Username</Form.Label>
-                            <Form.Control className={styles.Input} type="text" placeholder="username" name="username" />
+                            <Form.Control className={styles.Input} type="text" placeholder="Username" name="username" value={username} onChange={handleChange} />
                         </Form.Group>
 
                         <Form.Group controlId="password1">
                             <Form.Label className="d-none">Password</Form.Label>
-                            <Form.Control className={styles.Input} type="password" placeholder="Password" name="password1" />
+                            <Form.Control className={styles.Input} type="password" placeholder="Password" name="password1" value={password1} onChange={handleChange} />
                         </Form.Group>
 
                         <Form.Group controlId="password2">
                             <Form.Label className="d-none">Confirm password</Form.Label>
-                            <Form.Control className={styles.Input} type="password" placeholder="Confirm password" name="password2" />
+                            <Form.Control className={styles.Input} type="password" placeholder="Confirm password" name="password2" value={password2} onChange={handleChange} />
                         </Form.Group>
 
                         <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} variant="primary" type="submit">
