9cb2f4732f4c254bba41ebd5bcf615e7fba46176
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-03-12 19:29:08 +0000

    add error handling in login form

--- src/pages/auth/LoginForm.js
+++ src/pages/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
@@ -8,36 +8,76 @@ import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
+import axios from "axios";
+
 function SignInForm() {
-    //   Add your component logic here
+    const [loginData, setLoginData] = useState({
+        username: '',
+        password: '',
+    });
+
+    const {username, password} = loginData;
+
+    const [errors, setErrors] = useState({});
+
+    const history = useHistory();
+
+    const handleChange = (event) => {
+        setLoginData({
+            ...loginData,
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post('/dj-rest-auth/login/', loginData);
+            history.push("/");
+        } catch (err) {
+            setErrors(err.response?.data);
+        }
+    }
 
     return (
         <Row className={styles.Row}>
             <Col className="my-auto p-0 p-md-2" md={6}>
                 <Container className={`${appStyles.Content} p-4 `}>
-                    <h1 className={styles.Header}>sign in</h1>
+                    <h1 className={styles.Header}>Login</h1>
 
                     {/* Login Form */}
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group controlId="username">
                             <Form.Label className="d-none">Username</Form.Label>
-                            <Form.Control className={styles.Input} type="text" placeholder="Username" name="username" />
+                            <Form.Control className={styles.Input} type="text" placeholder="Username" name="username" value={username} onChange={handleChange} />
                         </Form.Group>
+                        {errors.username?.map((message, idx) =>(
+                        <Alert variant="warning" key={idx}>{message}
+                        </Alert>
+                        ))}
 
                         <Form.Group controlId="password">
                             <Form.Label className="d-none">Password</Form.Label>
-                            <Form.Control className={styles.Input} type="password" placeholder="Password" name="password" />
+                            <Form.Control className={styles.Input} type="password" placeholder="Password" name="password" value={password} onChange={handleChange} />
                         </Form.Group>
+                        {errors.password?.map((message, idx) =>(
+                            <Alert variant="warning" key={idx}>{message}
+                            </Alert>
+                            ))}
 
                         <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} type="submit">
                             Login
                         </Button>
+                        {errors.non_field_errors?.map((message, idx) => (
+                            <Alert variant="warning" className="mt-3" key={idx}>{message}
+                            </Alert>
+                            ))}
                     </Form>
 
                 </Container>
