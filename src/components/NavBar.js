edb104f2b860c2b0b39c8be7a89cb0c6853442af
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-03-13 10:49:19 +0000

    add username displayed when user is logged in

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/logo.jpeg';
 import styles from '../styles/NavBar.module.css';
 import { NavLink } from 'react-router-dom';
+import { CurrentUserContext } from '../App';
 
 const NavBar = () => {
-  return (
-  <Navbar expand="md" fixed="top" className={styles.NavBar}>
-    <Container>
-      <NavLink to='/'>
-      <Navbar.Brand>
-        <img src={logo} alt='logo' height='45' />
-      </Navbar.Brand>
+  const currentUser = useContext(CurrentUserContext);
+  const loggedInIcons = <>{currentUser?.username}</>
+  const loggedOutIcons = (
+    <>
+      <NavLink to='/login' activeClassName={styles.Active}>
+        <i className="fas fa-door-open"></i> LOGIN
       </NavLink>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ms-auto">
-          <NavLink exact to='/' activeClassName={styles.Active}>
-            <i className="fas fa-dharmachakra"></i> HOME
-          </NavLink>
-          <NavLink to='/login' activeClassName={styles.Active}>
-            <i className="fas fa-door-open"></i> LOGIN
-          </NavLink>
-          <NavLink to='/signup' activeClassName={styles.Active}>
-            <i className="fas fa-user-plus"></i> SIGN UP
-          </NavLink>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
+      <NavLink to='/signup' activeClassName={styles.Active}>
+        <i className="fas fa-user-plus"></i> SIGN UP
+      </NavLink>
+    </>
+  );
+  return (
+    <Navbar expand="md" fixed="top" className={styles.NavBar}>
+      <Container>
+        <NavLink to='/'>
+          <Navbar.Brand>
+            <img src={logo} alt='logo' height='45' />
+          </Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <NavLink exact to='/' activeClassName={styles.Active}>
+              <i className="fas fa-dharmachakra"></i> HOME
+            </NavLink>
+            {currentUser ? loggedInIcons : loggedOutIcons}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
--- src/pages/auth/LoginForm.js
+++ src/pages/auth/LoginForm.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import axios from "axios";
 
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
@@ -13,10 +14,12 @@ import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import { SetCurrentUserContext } from '../../App';
 
-import axios from "axios";
 
 function SignInForm() {
+    const setCurrentUser = useContext(SetCurrentUserContext);
+
     const [loginData, setLoginData] = useState({
         username: '',
         password: '',
@@ -30,7 +33,8 @@ function SignInForm() {
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            await axios.post('/dj-rest-auth/login/', loginData);
+            const {data} = await axios.post('/dj-rest-auth/login/', loginData);
+            setCurrentUser(data.user);
             history.push("/");
         } catch (err) {
             setErrors(err.response?.data);
