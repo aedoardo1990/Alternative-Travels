c475c45f1b4962ad5d244d977ae9356d01916350
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-03-19 11:34:34 +0000

    modify NavBar burger toggle with custom hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,10 +6,13 @@ import { NavLink } from 'react-router-dom';
 import { useCurrentUser, useSetCurrentUser, } from '../contexts/CurrentUserContext';
 import Avatar from './Avatar';
 import axios from 'axios';
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -27,20 +30,20 @@ const NavBar = () => {
   );
 
   const loggedInIcons = (
-  <>
-    <NavLink to='/newsfeed' className={styles.NavLink} activeClassName={styles.Active}>
-      <i className="fas fa-regular fa-newspaper"></i> Newsfeed
-    </NavLink>
-    <NavLink to='/liked-posts' className={styles.NavLink} activeClassName={styles.Active}>
-      <i className="fas fa-solid fa-thumbs-up"></i> Liked
-    </NavLink>
-    <NavLink to='/' className={styles.NavLink} onClick={handleSignOut}>
-      <i className="fas fa-solid fa-right-from-bracket"></i> Logout
-    </NavLink>
-    <NavLink to={`/profiles/${currentUser?.profile_id}`} className={styles.NavLink}>
-      <Avatar src={currentUser?.profile_image} text={currentUser?.username} height={40} />
-    </NavLink>
-  </>
+    <>
+      <NavLink to='/newsfeed' className={styles.NavLink} activeClassName={styles.Active}>
+        <i className="fas fa-regular fa-newspaper"></i> Newsfeed
+      </NavLink>
+      <NavLink to='/liked-posts' className={styles.NavLink} activeClassName={styles.Active}>
+        <i className="fas fa-solid fa-thumbs-up"></i> Liked
+      </NavLink>
+      <NavLink to='/' className={styles.NavLink} onClick={handleSignOut}>
+        <i className="fas fa-solid fa-right-from-bracket"></i> Logout
+      </NavLink>
+      <NavLink to={`/profiles/${currentUser?.profile_id}`} className={styles.NavLink}>
+        <Avatar src={currentUser?.profile_image} text={currentUser?.username} height={40} />
+      </NavLink>
+    </>
   );
   const loggedOutIcons = (
     <>
@@ -53,7 +56,7 @@ const NavBar = () => {
     </>
   );
   return (
-    <Navbar expand="md" fixed="top" className={styles.NavBar}>
+    <Navbar expanded={expanded} expand="md" fixed="top" className={styles.NavBar}>
       <Container>
         <NavLink to='/'>
           <Navbar.Brand>
@@ -61,7 +64,7 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle ref={ref} onClick={() => setExpanded(!expanded)} aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <NavLink exact to='/' activeClassName={styles.Active}>
