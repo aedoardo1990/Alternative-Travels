09f02dd12a3972e32e8fe9196dedf9a39a49a8c7
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-03-20 19:34:42 +0000

    create CommentCreateForm.js to write comments under posts

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -8,25 +8,31 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function PostPage() {
-    const {id} = useParams();
-    const [post, setPost] = useState({ results: [] });
+  const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
 
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                const [{ data: post }] = await Promise.all([
-                    axiosReq.get(`/posts/${id}`),
-                ]);
-                setPost({ results: [post] });
-                console.log(post);
-            } catch (err) {
-                console.log(err);
-            }
-        };
-        handleMount();
-    }, [id]);
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    handleMount();
+  }, [id]);
 
   return (
     <Row className="h-100">
@@ -34,7 +40,17 @@ function PostPage() {
         <p>Popular profiles for mobile</p>
         <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>
-          Comments
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
