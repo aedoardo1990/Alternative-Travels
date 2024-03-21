90d4ae6bdf9905d6a478504c003d8b170bb8dad7
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-03-21 10:37:53 +0000

    add operative functionality to edit comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from "react";
 import { Media } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { MoreDropdown } from '../../components/MoreDropdown';
+import CommentEditForm from "./CommentEditForm";
 import styles from "../../styles/Comment.module.css";
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { axiosRes } from '../../api/axiosDefaults';
@@ -19,6 +20,7 @@ const Comment = (props) => {
     setComments,
   } = props;
 
+  const [showEditForm, setShowEditForm] = useState(false);
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
 
@@ -28,14 +30,14 @@ const Comment = (props) => {
       setPost((prevPost) => ({
         results: [
           {
-        ...prevPost.results[0],
-        comments_count: prevPost.results[0].comments_count - 1,
-        },
-      ], 
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count - 1,
+          },
+        ],
       }));
 
-      setComments((prevComments) =>({
-        prevComments,
+      setComments((prevComments) => ({
+        ...prevComments,
         results: prevComments.results.filter((comment) => comment.id !== id),
       }));
     } catch (err) {
@@ -45,22 +47,36 @@ const Comment = (props) => {
 
 
   return (
-    <div>
+    <>
       <hr />
       <Media>
         <Link to={`/profiles/${profile_id}`}>
           <Avatar src={profile_image} />
         </Link>
-        <Media.Body className='align-self-center ml-2'>
+        <Media.Body className="align-self-center ml-2">
           <span className={styles.Owner}>{owner}</span>
           <span className={styles.Date}>{updated_at}</span>
-          <p>{content}</p>
+          {showEditForm ? (
+            <CommentEditForm
+            id={id}
+            profile_id={profile_id}
+            content={content}
+            profileImage={profile_image}
+            setComments={setComments}
+            setShowEditForm={setShowEditForm}
+          />
+          ) : (
+            <p>{content}</p>
+          )}
         </Media.Body>
-        {is_owner && (
-          <MoreDropdown handleDelete={handleDelete}/>
+        {is_owner && !showEditForm && (
+          <MoreDropdown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
+          />
         )}
       </Media>
-    </div>
+    </>
   );
 };
 
