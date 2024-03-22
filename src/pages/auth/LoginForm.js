106eb6887eeb8b2958c9d682d86706e7380bdbc2
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-03-22 15:12:50 +0000

    add user redirect to allow/not allow functionalities if user is loggedIn/Out

--- src/pages/auth/LoginForm.js
+++ src/pages/auth/LoginForm.js
@@ -15,9 +15,11 @@ import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import { useSetCurrentUser } from '../../contexts/CurrentUserContext';
+import { useRedirect } from "../../hooks/useRedirect";
 
 function SignInForm() {
     const setCurrentUser = useSetCurrentUser();
+    useRedirect('loggedIn');
 
     const [loginData, setLoginData] = useState({
         username: '',
@@ -34,7 +36,7 @@ function SignInForm() {
         try {
             const {data} = await axios.post('/dj-rest-auth/login/', loginData);
             setCurrentUser(data.user);
-            history.push("/");
+            history.goBack();
         } catch (err) {
             setErrors(err.response?.data);
         }
