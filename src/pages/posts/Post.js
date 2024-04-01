6d2abf82be31f1c55de07e9c0c60684a5eaf474f
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-04-01 17:10:08 +0000

    style name of users

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -131,8 +131,8 @@ const Post = (props) => {
             <Card className={styles.Post}>
                 <Card.Body>
                     <Media className='align-items-center justify-content-between'>
-                        <Link to={`/profiles/${profile_id}`}>
-                            <Avatar src={profile_image} height={55} />
+                        <Link to={`/profiles/${profile_id}`} className="fw-bold">
+                            <Avatar src={profile_image} height={40}/>
                             {owner}
                         </Link>
                         <div className='d-flex align-items-center'>
