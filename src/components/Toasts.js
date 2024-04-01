ee3cfddfbe4454ae2b269300e0bc66176dc884dc
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-04-01 12:08:39 +0000

    add credits to Toasts.js

--- src/components/Toasts.js
+++ src/components/Toasts.js
@@ -1,6 +1,7 @@
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+//credits to https://github.com/samueldjones24/code-anagrams/tree/main
 toast.configure({ autoClose: 2000, position: toast.POSITION.BOTTOM_CENTER });
 
 const successToast = (message, options = {}) =>
