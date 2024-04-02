22e935ba387fee0ef256fbeccc29b0c39d20f46a
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-04-02 08:19:54 +0000

    adaptwidth of videeos to 100%

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -166,7 +166,7 @@ const Post = (props) => {
                     </Link>
                 ) : (
                     <Link to={`/posts/${id}`}>
-                        <video src={video} width={290} height={400} controls />
+                        <video src={video} controls width="100%"/>
                     </Link>
                 )}
 
