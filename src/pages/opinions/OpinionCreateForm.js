c815cbdb9b70e30e8a9f936b48ac2f7665ede00d
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-04-04 18:03:46 +0000

    create OpinionCreateForm.js

---
+++ src/pages/opinions/OpinionCreateForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+
+import styles from "../../styles/CommentCreateEditForm.module.css";
+import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
+
+function OpinionCreateForm(props) {
+  const { marketplace, setMarketplace, setOpinions, profileImage, profile_id } = props;
+  const [content, setContent] = useState("");
+
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axiosRes.post("/opinions/", {
+        content,
+        marketplace,
+      });
+      setOpinions((prevOpinions) => ({
+        ...prevOpinions,
+        results: [data, ...prevOpinions.results],
+      }));
+      setMarketplace((prevMarketplace) => ({
+        results: [
+          {
+            ...prevMarketplace.results[0],
+            opinions_count: prevMarketplace.results[0].opinions_count + 1,
+          },
+        ],
+      }));
+      setContent("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Form className="mt-2" onSubmit={handleSubmit}>
+      <Form.Group>
+        <InputGroup>
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profileImage} />
+          </Link>
+          <Form.Control
+            className={styles.Form}
+            placeholder="Write a comment..."
+            as="textarea"
+            value={content}
+            onChange={handleChange}
+            rows={2}
+          />
+        </InputGroup>
+      </Form.Group>
+      <button
+        className={`${styles.Button} btn d-block ml-auto`}
+        disabled={!content.trim()}
+        type="submit"
+      >
+        Post<i class="fa-brands fa-telegram"></i>
+      </button>
+    </Form>
+  );
+}
+
+export default OpinionCreateForm;
