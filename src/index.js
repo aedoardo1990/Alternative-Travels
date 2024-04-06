662e24a49205ae1212c1045670df5a814fbd8ee8
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-04-06 10:57:55 +0000

    change data in index.js because there was error in console

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,7 +8,8 @@ import { CurrentUserProvider } from './contexts/CurrentUserContext';
 import { ProfileDataProvider } from './contexts/ProfileDataContext';
 import 'leaflet/dist/leaflet.css'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Router>
       <CurrentUserProvider>
@@ -17,8 +18,7 @@ ReactDOM.render(
         </ProfileDataProvider>
       </CurrentUserProvider>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
